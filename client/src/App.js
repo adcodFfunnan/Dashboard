59fb7adc902c02d5f9f701268fb24ba21990267b
Adnan Birparic <adnan.birparic@gmail.com>
2021-03-22 06:44:30 +0100

    pushToGithub

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter as Router, Switch } from 'react-router-dom'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 import { Header } from './app/Header'
 import { VerticalMenu } from './app/menu/Menu'
